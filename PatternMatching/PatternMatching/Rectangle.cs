3953694ec73a5e2a3a9e7e4ea1b90bdfc17365bb
Ray-Jay <rdh_jns@outlook.com>
2019-10-21 10:30:01 -0700

    Add entire folder for PatternMatching practice

---
+++ PatternMatching/PatternMatching/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternMatching
+{
+    class Rectangle
+    {
+        // properties
+        public double Length { get; }
+        public double Height { get; }
+
+        // constructor
+        public Rectangle(double length, double height)
+        {
+            Length = length;
+            Height = height;
+        }
+    }
+}
---
+++ PatternMatching/PatternMatching/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternMatching
+{
+    class Square
+    {
+        // properties
+        public double Side { get; }
+
+        // constructor
+        public Square(double side)
+        {
+            Side = side;
+        }
+    }
+}
