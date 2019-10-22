8eb34fbb8ad4032dfe238a8b07fd04cfae8b5ac5
Ray-Jay <rdh_jns@outlook.com>
2019-10-22 09:31:48 -0700

    initial commit

---
+++ Events/Events/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    public class Point
+    {
+        // fields and properties
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get { return x; }
+            //set { x = value; }
+            set                 // change set to set value and cal OnPointChanged to see if event is raised
+            {
+                x = value;
+                OnPointChanged();
+            }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            //set { y = value; }
+            set
+            {
+                y = value;      // change set to set value and cal OnPointChanged to see if event is raised
+                OnPointChanged();
+            }
+        }
+
+        // event keyword defines this as event
+        // EventHandler is the name of the delegate
+        // PointChanged is the name of the event
+        public event EventHandler PointChanged;
+
+        public void OnPointChanged()            // code to raise event PointChanged
+        {
+            if (PointChanged != null)
+                PointChanged(this, EventArgs.Empty);        // required parameters for EventHandler delegate
+        }
+
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Date: 10-22-19
+//  Chapter 33 page 213 - 215
+//  Purpose: Practice creating events
+
+using System;
+
+namespace Events
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Point p = new Point();
+            p.PointChanged += HandlePointChanged;
+            Console.WriteLine($"({p.X},{p.Y})");
+            p.X = 3;
+            Console.WriteLine($"({p.X},{p.Y})");
+
+        }
+
+        public static void HandlePointChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Point Changed Handled");
+        }
+    }
+}
