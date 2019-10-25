e6a65afda7f2ce6633fa61b05c08b13c6b2742c0
Ray-Jay <rdh_jns@outlook.com>
2019-10-25 13:15:47 -0700

    Add files via upload

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,26 @@
+﻿//  Date: 10-25-19
+//  Exercism IO Difference of Squares
+//  Purpose: Finds the square of a sum, sum of squares, and the difference between the two
+
+using System;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    // uses formula to calculate 
+    public static int CalculateSquareOfSum(int max)
+    {
+        return (int)Math.Pow((max * (1 + max)) / 2, 2);
+    }
+
+    // uses LINQ to generate numbers, square them, then add them
+    public static int CalculateSumOfSquares(int max)
+    {
+        return Enumerable.Range(1, max).Select(x => x * x).Sum();
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
