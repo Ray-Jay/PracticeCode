7fc792f73547bd43780a23e3ed2699fc4d8ac6b0
Ray-Jay <rdh_jns@outlook.com>
2019-10-24 23:45:55 -0700

    Add files via upload

---
+++ QueryExpressions/QueryExpressions/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryExpressions
+{
+    public class Player
+    {
+        // properties
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string TeamColor { get; set; }
+    }
+}
