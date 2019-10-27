30282960814cb6c15c0e7faed80a5d725803da43
Ray-Jay <rdh_jns@outlook.com>
2019-10-27 14:50:55 -0700

    Add files via upload

---
+++ pangram/Pangram.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        return input.ToUpper().Where(ch => char.IsLetter(ch)).Distinct().Count() == 26;
+    }
+}
