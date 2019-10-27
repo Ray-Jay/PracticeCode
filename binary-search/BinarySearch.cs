3948efa23d08d72e8ecca1c83340a88771c29ff1
Ray-Jay <rdh_jns@outlook.com>
2019-10-27 14:04:14 -0700

    Add files via upload

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        int beginIndex = 0;
+        int midIndex = 0;
+        int lastIndex = input.Length - 1;
+
+        while (beginIndex <= lastIndex)
+        {
+            midIndex = (beginIndex + lastIndex) / 2;
+            if (input[midIndex] < value)
+            {
+                beginIndex = midIndex + 1;
+            }
+            else if (input[midIndex] > value)
+            {
+                lastIndex = midIndex - 1;
+            }
+            else    // value found
+            {
+                return midIndex;
+            }
+        }
+
+        return -1;
+    }
+}
