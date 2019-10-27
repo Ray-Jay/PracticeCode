d946112fb98b85b2447a6a72de359a41bf4bc8f9
Ray-Jay <rdh_jns@outlook.com>
2019-10-27 16:36:51 -0700

    Add files via upload

---
+++ etl/Etl.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Etl
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
+    {
+        Dictionary<string, int> transformed = new Dictionary<string, int>();
+
+        foreach (int point in old.Keys)
+        {
+            old.TryGetValue(point, out string[] valuesArr);
+            for (int i = 0; i < valuesArr.Length; i++)
+            {
+                transformed[valuesArr[i].ToLower()] = point;
+            }
+        }
+
+        return transformed;
+    }
+}
