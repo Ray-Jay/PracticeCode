cc87c99643260595e6c1f6df1c8c324b53e55acb
Ray-Jay <rdh_jns@outlook.com>
2019-12-05 23:23:29 -0800

    Add files via upload

---
+++ JavaScript/spiral-matrix.js
@@ -0,0 +1,43 @@
+export class SpiralMatrix {
+  static ofSize(size) {
+    let spiral = new Array(size);
+    for (var i = 0; i < size; i++) {
+      spiral[i] = [];
+    }
+    let totalElements = size * size;
+    let rowBegin = 0;
+    let rowEnd = size - 1;
+    let columnBegin = 0;
+    let columnEnd = size - 1;
+    let number = 1;
+
+    // populate the perimeter elements of the square then make the square smaller each iteration
+    // by moving begin and end bounds
+    while (number <= totalElements) {
+      // left to right on a row
+      for (let i = columnBegin; i <= columnEnd; i++) {
+        spiral[rowBegin][i] = number++;
+      }
+      rowBegin++;
+
+      // down a column
+      for (let i = rowBegin; i <= rowEnd; i++) {
+        spiral[i][columnEnd] = number++;
+      }
+      columnEnd--;
+
+      // right to left on a row
+      for (let i = columnEnd; i >= columnBegin; i--) {
+        spiral[rowEnd][i] = number++;
+      }
+      rowEnd--;
+
+      // up a column
+      for (let i = rowEnd; i >= rowBegin; i--) {
+        spiral[i][columnBegin] = number++;
+      }
+      columnBegin++;
+    }
+    return spiral;
+  }
+}
