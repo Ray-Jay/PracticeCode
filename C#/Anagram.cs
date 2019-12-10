be8d94b0f01eaa3872acc59f0a24359347d620f1
Ray-Jay <rdh_jns@outlook.com>
2019-12-09 21:27:58 -0800

    Add files via upload

---
+++ C#/Anagram.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Anagram
+{
+    // constructor
+    public Anagram(string baseWord)
+    {
+        OriginalWord = baseWord.ToLower();
+        OriginalSorted = new string(OriginalWord.OrderBy(c => c).ToArray());
+    }
+    // properties
+    public string OriginalWord { get; set; }
+    public string OriginalSorted { get; set; }
+
+    public string[] FindAnagrams(string[] potentialMatches)
+    {
+        List<string> result = new List<string>();
+        string checkWord = "";
+        string checkSorted = "";
+
+        // for each word in input array, sort it and compare to the original word
+        potentialMatches.ToList().ForEach(element =>
+        {
+            checkWord = element.ToLower();
+            checkSorted = new string(checkWord.OrderBy(c => c).ToArray());
+            if (checkSorted == OriginalSorted && OriginalWord != checkWord)
+            {
+                result.Add(element);
+            }
+        });
+
+        return result.ToArray();
+    }
+}
